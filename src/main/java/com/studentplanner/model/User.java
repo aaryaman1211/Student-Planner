65f3047455743f10cb62d3586ce4af18de186212
Aaryaman Parulekar <aaryamanparulekar@Aaryamans-MacBook-Air.local>
2026-04-03 00:21:30 +0530

    Add User model with encapsulation

---
+++ src/main/java/com/studentplanner/model/User.java
@@ -0,0 +1,33 @@
+package com.studentplanner.model;
+
+public class User {
+    private String uid;
+    private String email;
+    private String name; 
+
+    public User(){}
+
+    public User(String uid, String email, String name){
+        this.uid = uid;
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getUid()
+        { return uid; }
+    
+    public String getEmail()
+        { return email; }
+    
+    public String getName()            
+        { return name; }
+    
+    public void setUid(String uid)     
+        { this.uid = uid; }
+    
+    public void setEmail(String email) 
+        { this.email = email; }
+    
+    public void setName(String name)   
+        { this.name = name; }
+}
